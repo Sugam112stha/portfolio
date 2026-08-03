31c5cf167ad26dee880379a5646d9ef3830c37f8
Sugam Shrestha <sugamstha619@gmail.com>
2026-08-03 16:44:57 +0545

    Build responsive hero section

--- src/component/sections/Hero.tsx
+++ src/component/sections/Hero.tsx
@@ -1,7 +1,10 @@
+import { FiDownload } from "react-icons/fi";
+import ProfileImage from "../../assets/images/pic1.jpeg";
+
 const Hero = () => {
   return (
-    <section className="flex p-10 min-h-screen flex items-center">
-      <div className="max-w-7xl pt-16">
+    <section className="flex flex-1 p-10 min-h-screen items-center">
+      <div className="max-w-7xl mx-auto pt-16">
         <div className="space-y-6">
           <p className="text-lg text-blue-400 font-medium"> Hello, I'm</p>
 
@@ -17,8 +20,16 @@ const Hero = () => {
             ideas into clean, interactive, and visually appealing digital
             experiences while continuously learning new technologies.
           </p>
-        </div>
 
+          <div className="flex items-center gap-5">
+            <button className="px-6 py-3 rounded-lg bg-blue-600 font-medium text-white transition-all duration-500 hover:bg-blue-500 hover:scale-105"> Hire me </button>
+            <button className="flex items-center gap-2 px-6 py-3 rounded-lg bg-blue-600 font-medium text-white transition-all duration-500 hover:bg-blue-500 hover:scale-105"> <FiDownload /> Download CV </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="h-125 w-95 flex overflow-hidden rounded-3xl shadow-2xl transition-all duration-500 hover:scale-105">
+        <img src={ProfileImage} alt="Sugam Shrestha" className="h-full w-full object-cover"/>
       </div>
     </section>
   );
